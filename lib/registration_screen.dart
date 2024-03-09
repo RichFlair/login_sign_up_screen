14440587181d0349e0a7738556db259b9fd09004
Andrew Marfo <andrewmarfo23@gmail.com>
2024-03-09 03:49:04 +0000

    setting up the background color

--- lib/registration_screen.dart
+++ lib/registration_screen.dart
@@ -6,7 +6,19 @@ class RegistrationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Registration screen'),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xffB81736),
+              Color(0xff281537),
+            ],
+          ),
+        ),
+        child: const Text('Create Your\nAccount'),
+      ),
     );
   }
 }
